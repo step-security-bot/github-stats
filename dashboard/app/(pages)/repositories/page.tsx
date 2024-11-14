94ad06be6474dce2e210e8b336fe2631cf144910
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-14 23:03:33 +0000

    chore: Add More Default Pages (#246)
    # Pull Request
    
    ## Description
    
    This pull request adds new pages for repositories and user information
    to the dashboard application. The most important changes include the
    creation of two new components that render the respective pages.
    
    New page components:
    
    *
    [`dashboard/app/(pages)/repositories/page.tsx`](diffhunk://#diff-08608cf160258ca75586d00e5be82714b21d0e205e837646183ef8a8a3971690R1-R8):
    Created a new `RepositoriesPage` component that displays a heading and a
    paragraph indicating it is the repositories page.
    *
    [`dashboard/app/(pages)/user/page.tsx`](diffhunk://#diff-d1e560b59fa9c6773a5f220451ce74583fc85fe294a9049b95a055a79bd350afR1-R8):
    Created a new `UserPage` component that displays a heading and a
    paragraph indicating it is the user page.
    
    Fixes #245

---
+++ dashboard/app/(pages)/repositories/page.tsx
@@ -0,0 +1,8 @@
+export default function RepositoriesPage() {
+  return (
+    <>
+      <h1>Repositories</h1>
+      <p>This is the repositories page</p>
+    </>
+  );
+}
---
+++ dashboard/app/(pages)/user/page.tsx
@@ -0,0 +1,8 @@
+export default function UserPage() {
+  return (
+    <>
+      <h1>User</h1>
+      <p>This is the user page</p>
+    </>
+  );
+}
