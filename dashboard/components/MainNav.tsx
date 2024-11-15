cf1b8386280aad1cb670c8e488af1edd7853e9e5
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-15 22:27:22 +0000

    build: Remove About Page (#277)
    # Pull Request
    
    ## Description
    
    This pull request includes several changes to the `dashboard`
    application, primarily focused on removing the "About" page and its
    associated components and tests, as well as a minor update to the
    `MainNav` component.
    
    ### Removal of "About" Page and Associated Components:
    
    *
    [`dashboard/app/(pages)/about/__tests__/__snapshots__/page.spec.tsx.snap`](diffhunk://#diff-b4d95efaec76f5a80f87d90ab8b29c6bc43fc0570f2087892ca24de8125ae233L1-L3):
    Removed the Jest snapshot for the "About" page.
    *
    [`dashboard/app/(pages)/about/__tests__/page.spec.tsx`](diffhunk://#diff-72772f72009526553fd021bd2ce3a986ab3fe9b503f08255e3b365c832e05247L1-L14):
    Removed the test file for the "About" page.
    *
    [`dashboard/app/(pages)/about/page.tsx`](diffhunk://#diff-f42ae02759621a022537ba23fa733e99456db9acda47f5bba11db7ba5198117dL1-L11):
    Removed the "About" page component.
    
    ### Updates to `MainNav` Component:
    
    *
    [`dashboard/components/MainNav.tsx`](diffhunk://#diff-47cf26539839396dad433337fdeac68758714662f652641d3fe4064c4b41a06fL9-R9):
    Changed the `className` prop type to
    `Readonly<React.HTMLAttributes<HTMLElement>>`.
    *
    [`dashboard/components/MainNav.tsx`](diffhunk://#diff-47cf26539839396dad433337fdeac68758714662f652641d3fe4064c4b41a06fL21-L26):
    Removed the "About" link from the navigation menu.
    
    ### Updates to `HomePageCards` Component:
    
    *
    [`dashboard/components/ui/HomePageCards.tsx`](diffhunk://#diff-82fb14677067d322c7e57befde94ca257473572ba7fecba2868805a223657404L7-R7):
    Removed the card linking to the "About" page and adjusted the grid
    layout accordingly.
    
    Fixes #276

--- dashboard/components/MainNav.tsx
+++ dashboard/components/MainNav.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 export default function MainNav({
   className,
   ...props
-}: React.HTMLAttributes<HTMLElement>) {
+}: Readonly<React.HTMLAttributes<HTMLElement>>) {
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
@@ -17,12 +17,6 @@ export default function MainNav({
         className="text-sm font-medium transition-colors hover:text-primary"
       >
         Overview
-      </Link>
-      <Link
-        href="/about"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-      >
-        About
       </Link>
       <Link
         href="/user"
