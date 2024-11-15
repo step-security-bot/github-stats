e50912d9ac8e0297b33360cd4397732b383a8f3d
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-15 22:04:57 +0000

    feat: Add Repositories Page Content (#274)
    # Pull Request
    
    ## Description
    
    This pull request includes changes to improve the layout and styling of
    the repositories and repository detail pages. The most important changes
    include the addition of a `Link` component, the use of repository data
    from a JSON file, and the application of new styles to the pages.
    
    Enhancements to repositories page:
    
    * Added `Link` component to create navigable links for each repository.
    * Imported repository data from `repository_statistics.json` to
    dynamically generate the list of repositories.
    * Applied new styles to center the content and format the repository
    list.
    
    Enhancements to repository detail page:
    
    * Added `Link` component for additional navigation links.
    ([dashboard/app/(pages)/repository/[name]/page.tsxL2-R2](diffhunk://#diff-8018d7573da2c0793502f6a906948f6b14e46fcf4aeef4f9f33c2a6d47943b45L2-R2),
    [dashboard/app/(pages)/repository/[name]/page.tsxL27-R43](diffhunk://#diff-8018d7573da2c0793502f6a906948f6b14e46fcf4aeef4f9f33c2a6d47943b45L27-R43))
    * Applied new styles to center the content and format the repository
    details.
    ([dashboard/app/(pages)/repository/[name]/page.tsxL27-R43](diffhunk://#diff-8018d7573da2c0793502f6a906948f6b14e46fcf4aeef4f9f33c2a6d47943b45L27-R43))
    
    Fixes #270

--- dashboard/app/(pages)/repositories/page.tsx
+++ dashboard/app/(pages)/repositories/page.tsx
@@ -1,8 +1,31 @@
+import Link from "next/link";
+
+import repositories from "../../../data/repository_statistics.json";
+
 export default function RepositoriesPage() {
   return (
-    <>
-      <h1>Repositories</h1>
-      <p>This is the repositories page</p>
-    </>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "33%", textAlign: "left" }}>
+        <h1 style={{ fontSize: "2em", fontWeight: "bold", color: "#0070f3" }}>
+          Repositories
+        </h1>
+        <ul style={{ listStyleType: "disc" }}>
+          {repositories.map((repository) => (
+            <li key={repository.repository}>
+              <Link
+                href={`/repository/${repository.repository}`}
+                style={{
+                  color: "#0070f3",
+                  textDecoration: "underline",
+                  fontWeight: "bold",
+                }}
+              >
+                {repository.repository}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
--- dashboard/app/(pages)/repository/[name]/page.tsx
+++ dashboard/app/(pages)/repository/[name]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { CommitsPieChart } from "@/components/PieChart";
 
 import repositories from "../../../../data/repository_statistics.json";
@@ -24,13 +26,55 @@ export default function RepositoryPage({
     (repository: Repository) => repository.repository === name,
   ) as Repository;
   return (
-    <>
-      <h1>Repository Name {repository.repository}</h1>
-      <h2>Total Files {repository.total_files}</h2>
-      <h2>Total Commits {repository.total_commits}</h2>
-      <CommitsPieChart
-        chartData={{ user: "Jack", total: repository.total_commits }}
-      />
-    </>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ width: "33%", textAlign: "left" }}>
+        <h1 style={{ fontSize: "2em", fontWeight: "bold", color: "#0070f3" }}>
+          Repository Name {repository.repository}
+        </h1>
+        <h2>Total Files {repository.total_files}</h2>
+        <h2>Total Commits {repository.total_commits}</h2>
+        <CommitsPieChart
+          chartData={{ user: "Jack", total: repository.total_commits }}
+        />
+        <ul>
+          <li>
+            <Link
+              href="#"
+              style={{
+                color: "#0070f3",
+                textDecoration: "underline",
+                fontWeight: "bold",
+              }}
+            >
+              Link 1
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#"
+              style={{
+                color: "#0070f3",
+                textDecoration: "underline",
+                fontWeight: "bold",
+              }}
+            >
+              Link 2
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#"
+              style={{
+                color: "#0070f3",
+                textDecoration: "underline",
+                fontWeight: "bold",
+              }}
+            >
+              Link 3
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
